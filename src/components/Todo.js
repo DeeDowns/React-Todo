f587766ef8a4034e2209e1950ed5d353e07eb341
Dee <47228065+DeeDowns@users.noreply.github.com>
2020-08-03 17:55:43 -0400

    got css to line through completed item

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import './Todo.css'
 
 function Todo(props) {
     // console.log(props)
     return (
-        <div onClick={() => props.toggleItem(props.item.name)} className={`item${props.item.completed ? 'completed' : ''}`}>
+        <div onClick={() => props.toggleItem(props.item.name)} className={`item${props.item.completed ? ' completed' : ''}`}>
             <h4>{props.item.name}</h4>
         </div>
     )
